50bb98fc386f94e98404dd7de2948ec3374dbb4d
Muhammadhuzaifagoraya0 <muhammadhuzaifagoraya0@gmail.com>
2026-08-19 16:26:04 +0500

    new

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,9 +12,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en">
-      <body className="bg-white antialiased">
+      <body className="flex min-h-screen flex-col bg-white antialiased">
         <Header />
-        {children}
+        <main className="flex-1">{children}</main>
         <Footer />
       </body>
     </html>
