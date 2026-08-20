2b25996317b04f0d5b605249f11bdfd89b37137a
Muhammadhuzaifagoraya0 <muhammadhuzaifagoraya0@gmail.com>
2026-08-20 17:41:06 +0500

    new

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,5 @@
 import Contact from "@/components/Contact";
 
 export default function ContactPage() {
-  return (
-    <div className="flex flex-col">
-      <Contact />
-    </div>
-  );
+  return <Contact />;
 }
